8b5a457102791e32224aa3fe2068366905f7fdad
plbkppexo <worapha.c@ku.th>
2023-08-03 21:55:54 +0700

    lab05.1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,6 @@ module ku.cs {
     exports ku.cs.lab03javafx;
     exports ku.cs.controllers;
     opens ku.cs.controllers to javafx.fxml;
+    exports ku.cs.models;
+    opens ku.cs.models to javafx.base;
 }
